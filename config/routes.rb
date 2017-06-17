7e8bd449a29f0ba0ebe3feb2cd214be24056833a
Ngoc L Ha <sammy_nlh@hotmail.com>
2017-06-17 15:47:46 +0100

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  # maps request for URL / static_pages / home to the home action in controller
+  # get responds to get request
   get 'static_pages/home'
-
   get 'static_pages/help'
-
+  get 'static_pages/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,34 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @name = "sheFit"
+  end
+
+  test "should get root" do
+    get '/'
+    assert_response :success
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "#{@name} | Home"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "sheFit | Help"
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "sheFit | About"
   end
 
 end
+
+# first two test are verifying URL
+# assert_selector test for the presence of a particular HTML tag
